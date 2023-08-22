76bf63f654a3a5096b1b2188e959ea0075d37398
joesharratt1229 <118444587+joesharratt1229@users.noreply.github.com>
2023-08-22 13:58:20 +0100

    Update models.py

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -4,31 +4,29 @@ from .database import Base
 
 
 class ChatbotMeta(Base):
-    __tablename__ = "ChatbotMeta"
+    __tablename__ = "chatbotmeta"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     description = Column(String)
     is_active = Column(Boolean)
 
-    # ChatbotItems = relationship('ChatbotItem', back_populates='owner')
 
 
 class ChatbotItem(Base):
-    __tablename__ = "ChatbotItem"
+    __tablename__ = "chatbotitem"
 
     id = Column(Integer, primary_key=True, index=True)
     tone = Column(String, default="Informative and finish off on a positive note")
     audience = Column(String, default="Adults")
     contextual_information = Column(String, default=" ")
     input_query = Column(String)
-    ChatbotMeta = Column(Integer, ForeignKey("ChatbotMeta.id"))
+    ChatbotMeta = Column(Integer, ForeignKey("chatbotmeta.id"))
 
-    # owner = relationship('ChatbotMeta', back_populates='ChatbotItems')
 
 
 class PromptView(Base):
-    __tablename__ = "PromptView"
+    __tablename__ = "promptview"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
